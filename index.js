2fd8b901d3a03cf4dcd605c21c40dbf5332e558d
PeachScript <scdzwyxst@gmail.com>
2019-12-15 16:56:26 +0800

    feat: support datahub args if using in bigfish

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const DataHub = require('macaca-datahub');
 const datahubMiddleware = require('datahub-proxy-middleware');
+const yargs = require('yargs-parser');
 
 module.exports = (api, opts = {}) => {
   const { debug } = api;
@@ -12,6 +13,8 @@ module.exports = (api, opts = {}) => {
     showBoard: false,
     ...opts,
   };
+  const isBigfish = Boolean(process.env.BIGFISH_VERSION);
+  const args = yargs(process.argv.slice(2));
 
   const defaultDatahub = new DataHub({
     port: datahubConfig.port,
@@ -19,11 +22,14 @@ module.exports = (api, opts = {}) => {
 
   debug('datahubConfig');
   debug(datahubConfig);
-  api._beforeServerWithApp(({ app }) => {
-    datahubMiddleware(app)(datahubConfig);
-    defaultDatahub.startServer(datahubConfig).then(() => {
-      debug('datahub ready');
-      console.log('datahub ready');
+
+  if (!isBigfish || args.datahub) {
+    api._beforeServerWithApp(({ app }) => {
+      datahubMiddleware(app)(datahubConfig);
+      defaultDatahub.startServer(datahubConfig).then(() => {
+        debug('datahub ready');
+        console.log('datahub ready');
+      });
     });
-  });
+  }
 };

e3f1a04ec8a1cfbd32f89c9020016179c4098b49
达峰的夏天 <xudafeng@126.com>
2019-12-17 12:00:38 +0800

    chore: ci support (#5)

--- test/fixture/pages/index.js
+++ test/fixture/pages/index.js
@@ -1,9 +1,35 @@
+import * as React from "react";
+
 import styles from './index.css';
 
-export default function() {
-  return (
-    <div className={styles.normal}>
-      <h1>Page index</h1>
-    </div>
-  );
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'doing request...',
+    };
+  }
+
+  componentWillMount() {
+    fetch('/api/test1')
+      .then(function(response) {
+        if (response.status >= 400) {
+          throw new Error('Bad response from server');
+        }
+        return response.json();
+      })
+      .then(res => {
+        this.setState({
+          title: `value from mock: ${res.foo}`,
+        })
+      });
+  }
+
+  render() {
+    return (
+      <div className={styles.normal}>
+        <p>{this.state.title}</p>
+      </div>
+    );
+  }
 }

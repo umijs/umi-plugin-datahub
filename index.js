e3fc8037caac39eebcbd7b6346daf690c8cb5099
PeachScript <scdzwyxst@gmail.com>
2019-12-19 17:37:58 +0800

    revert: bigfish integrate logic in this major version

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 const path = require('path');
 const DataHub = require('macaca-datahub');
 const datahubMiddleware = require('datahub-proxy-middleware');
-const yargs = require('yargs-parser');
 
 module.exports = (api, opts = {}) => {
   const { debug } = api;
@@ -15,8 +14,6 @@ module.exports = (api, opts = {}) => {
     showBoard: false,
     ...opts,
   };
-  const isBigfish = Boolean(process.env.BIGFISH_VERSION);
-  const args = yargs(process.argv.slice(2));
 
   const defaultDatahub = new DataHub({
     port: datahubConfig.port,
@@ -24,26 +21,11 @@ module.exports = (api, opts = {}) => {
 
   debug('datahubConfig');
   debug(datahubConfig);
-
-  // register datahub args for dev command if integrated in Bigfish
-  if (isBigfish) {
-    api._modifyHelpInfo(memo => {
-      memo.commands.dev.opts.options = {
-        ...(memo.commands.dev.opts.options || {}),
-        '--datahub': 'start dev server & datahub server at the same time',
-      };
-
-      return memo;
+  api._beforeServerWithApp(({ app }) => {
+    datahubMiddleware(app)(datahubConfig);
+    defaultDatahub.startServer(datahubConfig).then(() => {
+      debug('datahub ready');
+      console.log('datahub ready');
     });
-  }
-
-  if (!isBigfish || args.datahub) {
-    api._beforeServerWithApp(({ app }) => {
-      datahubMiddleware(app)(datahubConfig);
-      defaultDatahub.startServer(datahubConfig).then(() => {
-        debug('datahub ready');
-        console.log('datahub ready');
-      });
-    });
-  }
+  });
 };

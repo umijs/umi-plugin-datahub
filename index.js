791a792562b9fa8052644a2a8032d40198d1af43
达峰的夏天 <xudafeng@126.com>
2019-12-29 19:34:20 +0800

    Next (#8)
    * build: upgrade to 4.0.0-beta.1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const path = require('path');
 const DataHub = require('macaca-datahub');
 const datahubMiddleware = require('datahub-proxy-middleware');
+const yargs = require('yargs-parser');
 
 module.exports = (api, opts = {}) => {
   const { debug } = api;
@@ -14,6 +15,8 @@ module.exports = (api, opts = {}) => {
     showBoard: false,
     ...opts,
   };
+  const isBigfish = Boolean(process.env.BIGFISH_VERSION);
+  const args = yargs(process.argv.slice(2));
 
   const defaultDatahub = new DataHub({
     port: datahubConfig.port,
@@ -21,13 +24,18 @@ module.exports = (api, opts = {}) => {
 
   debug('datahubConfig');
   debug(datahubConfig);
-  api._beforeServerWithApp(({ app }) => {
-    datahubMiddleware(app)(datahubConfig);
-    defaultDatahub.startServer(datahubConfig).then(() => {
-      debug('datahub ready');
-      console.log('datahub ready');
+
+  // register datahub args for dev command if integrated in Bigfish
+  if (isBigfish) {
+    api._modifyHelpInfo(memo => {
+      memo.commands.dev.opts.options = {
+        ...(memo.commands.dev.opts.options || {}),
+        '--datahub': 'start dev server & datahub server at the same time',
+      };
+
+      return memo;
     });
-  });
+  }
 
   api.onUISocket(({ action, success }) => {
     const { type } = action;
@@ -43,4 +51,14 @@ module.exports = (api, opts = {}) => {
 
   // add UI
   api.addUIPlugin(require.resolve('./dist/index.umd'));
+
+  if (!isBigfish || args.datahub) {
+    api._beforeServerWithApp(({ app }) => {
+      datahubMiddleware(app)(datahubConfig);
+      defaultDatahub.startServer(datahubConfig).then(() => {
+        debug('datahub ready');
+        console.log('datahub ready');
+      });
+    });
+  }
 };
